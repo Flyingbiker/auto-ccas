e802f71dc1e1588f82f222e9c1eaccf3f1776b37
Flyingbiker <schoebel.aymeric@gmail.com>
2021-03-18 22:12:45 +0100

    ajout de l'interface garage

---
+++ src/app/interfaces/garage.d.ts
@@ -0,0 +1,10 @@
+export interface Garage {
+    id_user : number;
+    name : string;
+    phone : string;
+    date_creation_garage : Date;
+    street_number: number;
+    street : string;
+    city : string;
+    num_dep : number;
+}
