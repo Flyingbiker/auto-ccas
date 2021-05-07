12d95a9df83317cd4a86ec7b9a64c4fca600148f
Flyingbiker <schoebel.aymeric@gmail.com>
2021-05-07 11:13:40 +0200

    Car mis et retiré du tableau en fonction des checkbox

--- src/app/components/card-car/card-car.component.ts
+++ src/app/components/card-car/card-car.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AnnonceService } from 'src/app/services/annonce/annonce.service';
+import { CompareCarsService } from 'src/app/services/compare-cars/compare-cars.service';
 
 @Component({
   selector: 'app-card-car',
@@ -13,7 +14,10 @@ export class CardCarComponent implements OnInit {
 
   @Input() annonce : Car;
 
+  public carChecked = false;
+
   constructor(private router : Router, 
+              private compareCarsService: CompareCarsService,
               private annonceService : AnnonceService) { }
 
   ngOnInit(): void {
@@ -25,5 +29,48 @@ export class CardCarComponent implements OnInit {
   public storeCarDetails(car : Car) {
     this.annonceService.storeCarDetails(car);
     this.router.navigate(['/car-details']);
+  }
+
+  public get checked() :boolean {
+    if (this.carChecked !== undefined) {
+      if (this.carChecked) {
+        // console.log("true : ", this.carChecked, this.annonce);      
+      } else {
+        // console.log("false : ", this.carChecked, this.annonce);            
+      }
+    }
+    return this.carChecked;
+  }
+  public set checked(value : boolean) {
+    this.carChecked = value;
+    if (value) {
+      if ( this.compareCarsService.carsToCompare.includes(this.annonce)){
+        console.error("car déja dans tableau alors que pas check" + value);
+      } else {
+        this.compareCarsService.carsToCompare.push(this.annonce);
+        console.log("car mis dans tableau ", this.compareCarsService.carsToCompare);
+      }
+    } else {
+      if ( this.compareCarsService.carsToCompare.includes(this.annonce)){
+        this.deleteCarFromCompareArray(this.annonce, this.compareCarsService.carsToCompare);
+        console.log("car retiré du tableau ", this.compareCarsService.carsToCompare);
+      } else {
+        console.error("car devait être retiré du tableau mais il n'y était plus");        
+      }
+    }
+
+  }
+
+  public deleteCarFromCompareArray(car : Car, array : Array<Car>) : Array<Car> {
+    let len = array.length;
+    let i = len-1;
+    for (i ; i>=0 ; i--){
+      if (car === array[i]){
+        array.splice(i,1);
+      } else {
+        console.log("valeur différente !");        
+      }
+    }
+    return array;
   }
 }
